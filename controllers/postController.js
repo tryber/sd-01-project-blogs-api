754fa1f9ebe4c0c97f3665ff9d01383a8fb85b1e
antoniopaolillo <antonio_francisco_p@live.com>
2020-07-31 15:41:37 -0300

    finish project

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,18 +8,53 @@ const createPost = async (req, res) => {
   const { title, content } = req.body;
   const { email } = req.user;
   if (!title || !content)
-    return res.status(500);
+    return res.status(500).json({ message: 'unauthorized' });
   const postCreated = await PostModel.createPost(title, content, email);
   return res.status(201).json(postCreated);
 }
 
-const getAllPosts = async (req, res) => {
+const getAllPosts = async (_req, res) => {
   const posts = await PostModel.getAllPosts();
   return res.status(200).json(posts);
 }
 
+const updatePost = async (req, res) => {
+  const { title, content } = req.body;
+  const { id } = req.params;
+  const { email } = req.user;
+  if (!title || !content)
+    return res.status(500).json({ message: 'data fail' });
+  const updatedPost = await PostModel.updatePost(title, content, email, id);
+  if (!updatedPost) return res.status(500).json({ message: 'unauthorized' });
+  return res.status(200).json({ message: 'post updated!' });
+}
+
+const getPost = async (req, res) => {
+  const { id } = req.params;
+  const post = await PostModel.getPost(id);
+  return res.status(200).json(post);
+}
+
+const deletePost = async (req, res) => {
+  const { email } = req.user;
+  const { id } = req.params;
+  const { message, status } = await PostModel.deletePost(email, id);
+  return res.status(status).json({ message });
+}
+
+const getBySearch = async (req, res) => {
+  const search = req.query.q;
+  const posts = await PostModel.getSearch(search);
+  res.status(200).json(posts);
+}
+
 router.get('/', rescue(getAllPosts));
+router.get('/search', rescue(getBySearch));
+router.get('/:id', rescue(getPost));
 router.use(validateJWT);
+
+router.delete('/:id', rescue(deletePost));
+router.put('/:id', rescue(updatePost));
 router.post('/', rescue(createPost));
 
 module.exports = router;
--- infrastructure/post/index.js
+++ infrastructure/post/index.js
@@ -1,4 +1,5 @@
 const { User, BlogPost } = require('../database/models');
+const { Op } = require('sequelize');
 const UserModel = require('../user');
 
 class PostModel {
@@ -21,6 +22,53 @@ class PostModel {
       include: [User]
     });
     return PostModel.formateObj(posts);
+  }
+
+  static async validateUserAndPost(email, id) {
+    const user = await UserModel.getUserByEmail(email);
+    const post = await BlogPost.findOne({ where: { id } });
+    if (!post || post.dataValues.user_id !== user.id) return false;
+    return true;
+  }
+
+  static async updatePost(title, content, email, id) {
+    const shouldUpdatePost = await PostModel.validateUserAndPost(email, id);
+    if (!shouldUpdatePost) return false;
+    const updatedPost = await BlogPost.update({ title, content, updated: Date.now() }, { where: { id } });
+    return updatedPost[0];
+  }
+
+  static async getPost(id) {
+    const { dataValues } = await BlogPost.findOne({ where: { id } });
+    return dataValues;
+  }
+
+  static async deletePost(email, id) {
+    const shouldUpdatePost = await PostModel.validateUserAndPost(email, id);
+    if (!shouldUpdatePost) return { status: 401, message: 'unauthorized' };
+    await BlogPost.destroy({ where: { id } });
+    return { status: 200, message: 'post deleted' };
+  }
+
+  static async getSearch(search) {
+    const posts = await BlogPost.findAll({
+      include: [User],
+      where: {
+        [Op.or]: [{
+          title: {
+            [Op.regexp]: search,
+          }
+        },
+        {
+          content: {
+            [Op.regexp]: search,
+          }
+        }],
+      },
+    });
+
+    if (!posts.length) throw new Error('SequelizeRegexPostNotFound');
+    return posts;
   }
 }
 
