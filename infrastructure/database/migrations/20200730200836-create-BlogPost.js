8327d97f18b6b15b42610922424ee58894020109
Guilherme Luiz <guiiluiz44@gmail.com>
2020-08-03 14:29:46 -0300

    codeclimate errors

--- infrastructure/database/migrations/20200730200836-create-BlogPost.js
+++ infrastructure/database/migrations/20200730200836-create-BlogPost.js
@@ -1,40 +1,44 @@
+const { sequelize } = require("../models");
+
+const obj = (Sequelize) => ({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  published: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: new Date(),
+  },
+  updated: {
+    type: Sequelize.DATE,
+    allowNull: false,
+    defaultValue: new Date(),
+  },
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id',
+    },
+    onDelete: 'CASCADE'
+  },
+})
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('BlogPosts', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      content: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      published: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: new Date(),
-      },
-      updated: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: new Date(),
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id',
-        },
-        onDelete: 'CASCADE'
-      },
-    });
+    return queryInterface.createTable('BlogPosts', obj(Sequelize));
   },
 
   down: async (queryInterface, Sequelize) => {
